3cdb822ed8ae47a87f00b83446515759e6874658
gioan777 <georgalexin@gmail.com>
2026-02-04 15:26:51 +0200

    fix some minor bug and cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,12 @@ mod spamotoniko;
 use spamotoniko::spamotoniko::update_spamotonics;
 use spamotoniko::spamotoniko::SpamotonicSystem;
 
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    //let mut line = String::new();
-    //let _ = std::io::stdin().read_line(&mut line).unwrap();
-
-    //println!("{}", katharevousopoisis(&line, &vowels_map));
-
-    /*
-      let native_options = eframe::NativeOptions{
-          viewport: egui::ViewportBuilder::default()
-             .with_inner_size([400.0, 300.0])
-             .with_min_inner_size([300.0, 220.0])
-             //.with_icon()
-             ,
-          ..Default::default()
-      };
-
-      eframe::run_native(
-          "eframe template",
-          native_options,
-          Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-      )
-    */
-
-    //let app = SpamotonicSystem {vowels_map};
+    use_eframe();
+}
+#[cfg(not(target_arch = "wasm32"))]
+fn use_eframe() {
     let native_options = eframe::NativeOptions::default();
-    //Text::run(Settings::default())
     let _ = eframe::run_native(
         "Σπαμοτονικό Σύστημα",
         native_options,
@@ -37,11 +16,9 @@ fn main() {
     );
 }
 #[cfg(target_arch = "wasm32")]
-fn main() {
+fn use_eframe() {
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
     let web_options = eframe::WebOptions::default();
-
     wasm_bindgen_futures::spawn_local(async {
         eframe::WebRunner::new()
             .start(
@@ -55,10 +32,11 @@ fn main() {
 }
 
 impl App for SpamotonicSystem {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         //let mut text=katharevousopoisis(input, &self.vowels_map);
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
             ui.heading("Σπαμοτονικό Σύστημα");
+            let old_string = self.input.clone();
             let response = ui.add(
                 egui::TextEdit::multiline(&mut self.input)
                     .font(egui::TextStyle::Monospace)
@@ -66,10 +44,10 @@ impl App for SpamotonicSystem {
                     .desired_width(f32::INFINITY),
             );
             ui.heading(&self.text);
-            if response.changed() {
+            if response.changed() && old_string != *self.input {
                 //text = katharevousopoisis(input, &self.vowels_map).clone();
-                update_spamotonics(&mut self.text, &mut self.input, &self.vowels_map);
-                ctx.request_repaint();
+                update_spamotonics(&mut self.text, &self.input, &self.vowels_map);
+                ui.request_repaint();
             }
         });
     }
